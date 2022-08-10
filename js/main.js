ca395451e703a4e75e66a216039756b7f9c3104f
YvonneOZertuche <aymzertuche1@outlook.com>
2022-08-10 09:52:48 -0500

    copy code to main.js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let button = document.querySelector('.button')
+const jokeDiv = document.querySelector('joke')
+let jokes = ''
+
+// li.append(".joke")
+const joke = async () => {
+  try {
+    const response = await fetch(
+      `https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw`
+    )
+    // const response = await fetch(`https://backend-omega-seven.vercel.app/api/getjoke`)
+    const data = await response.json()
+    const jokes = data[0]
+    // console.log(data[0])
+    console.log(jokes)
+  } catch (e) {
+    console.log('ERROR!', e)
+  }
+}
+
+button.addEventListener('click', e => {
+  joke()
+})
+
+function displayJoke (data) {
+  const jokes = data.object[0]
+}
