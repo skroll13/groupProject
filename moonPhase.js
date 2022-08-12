d3e244c1f7ae7510294566a5ea271a84e9f87264
Nauman Khan <thetopofsoup@gmail.com>
2022-08-12 11:10:58 -0400

    new changes to website

--- moonPhase.js
+++ moonPhase.js
@@ -1,17 +1,34 @@
-// this is where we'll have the code for the moon phase api pull
 
+
+// this is where we'll have the code for the moon phase api pull
 
 let dateInput = document.querySelector('#dateInput')
 let searchButton = document.querySelector('#search')
 let state = document.getElementById('state');
 let displayMoonPhase = document.querySelector(".displayMoonPhase")
+const displayJoke = document.querySelector(".displayJoke")
+let htmlFragment = ''
+
+const createFragment1 = jokeData => {
+    if (jokeData.type == 'single'){
+    htmlFragment =`<p>${jokeData.joke}</p>`
+    console.log(jokeData.type)
+    console.log(htmlFragment)
+    }else if 
+      (jokeData.type == 'twopart'){
+        htmlFragment = `<p>${jokeData.setup} ${jokeData.delivery}</p>`
+        console.log(jokeData.type)
+        console.log(htmlFragment)
+      }
+    displayJoke.innerHTML = htmlFragment
+}
 
 searchButton.addEventListener('click', (e) => {
         let dateOutput = dateInput.value
         let stateOutput = state.value
         console.log(stateOutput)
-
     moonMain(dateOutput, stateOutput)
+    joke()
 })
 
 const createFragment = (data, moonText) =>{
@@ -66,7 +83,6 @@ const moonPhaseConverter = (moonDecimal) => {
         }
     return moonPhaseWords    
 } 
-
 
 var usStates = [
     { name: 'ALABAMA', abbreviation: 'AL'},
@@ -137,97 +153,15 @@ for(var i = 0; i < usStates.length; i++) {
     state.add(option);
 }
 
-// Veronica T's code
-// let data;
-//     const getData = async (date,state) => { //main function
-//         try {
-//             let fetchData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${state}/${date}?unitGroup=us&key=PXLCYVMHK8W88EN9J7UFB2NFN&include=days&elements=moonphase,sunrise,sunset&contentType=json`)
-//             let result = await fetchData.json();
-//             data = result
-
-//         } catch (error) {
-//             console.log(error)
-//         }
-
-    
-
-
-
-var usStates = [
-    { name: 'ALABAMA', abbreviation: 'AL'},
-    { name: 'ALASKA', abbreviation: 'AK'},
-    { name: 'AMERICAN SAMOA', abbreviation: 'AS'},
-    { name: 'ARIZONA', abbreviation: 'AZ'},
-    { name: 'ARKANSAS', abbreviation: 'AR'},
-    { name: 'CALIFORNIA', abbreviation: 'CA'},
-    { name: 'COLORADO', abbreviation: 'CO'},
-    { name: 'CONNECTICUT', abbreviation: 'CT'},
-    { name: 'DELAWARE', abbreviation: 'DE'},
-    { name: 'DISTRICT OF COLUMBIA', abbreviation: 'DC'},
-    { name: 'FEDERATED STATES OF MICRONESIA', abbreviation: 'FM'},
-    { name: 'FLORIDA', abbreviation: 'FL'},
-    { name: 'GEORGIA', abbreviation: 'GA'},
-    { name: 'GUAM', abbreviation: 'GU'},
-    { name: 'HAWAII', abbreviation: 'HI'},
-    { name: 'IDAHO', abbreviation: 'ID'},
-    { name: 'ILLINOIS', abbreviation: 'IL'},
-    { name: 'INDIANA', abbreviation: 'IN'},
-    { name: 'IOWA', abbreviation: 'IA'},
-    { name: 'KANSAS', abbreviation: 'KS'},
-    { name: 'KENTUCKY', abbreviation: 'KY'},
-    { name: 'LOUISIANA', abbreviation: 'LA'},
-    { name: 'MAINE', abbreviation: 'ME'},
-    { name: 'MARSHALL ISLANDS', abbreviation: 'MH'},
-    { name: 'MARYLAND', abbreviation: 'MD'},
-    { name: 'MASSACHUSETTS', abbreviation: 'MA'},
-    { name: 'MICHIGAN', abbreviation: 'MI'},
-    { name: 'MINNESOTA', abbreviation: 'MN'},
-    { name: 'MISSISSIPPI', abbreviation: 'MS'},
-    { name: 'MISSOURI', abbreviation: 'MO'},
-    { name: 'MONTANA', abbreviation: 'MT'},
-    { name: 'NEBRASKA', abbreviation: 'NE'},
-    { name: 'NEVADA', abbreviation: 'NV'},
-    { name: 'NEW HAMPSHIRE', abbreviation: 'NH'},
-    { name: 'NEW JERSEY', abbreviation: 'NJ'},
-    { name: 'NEW MEXICO', abbreviation: 'NM'},
-    { name: 'NEW YORK', abbreviation: 'NY'},
-    { name: 'NORTH CAROLINA', abbreviation: 'NC'},
-    { name: 'NORTH DAKOTA', abbreviation: 'ND'},
-    { name: 'NORTHERN MARIANA ISLANDS', abbreviation: 'MP'},
-    { name: 'OHIO', abbreviation: 'OH'},
-    { name: 'OKLAHOMA', abbreviation: 'OK'},
-    { name: 'OREGON', abbreviation: 'OR'},
-    { name: 'PALAU', abbreviation: 'PW'},
-    { name: 'PENNSYLVANIA', abbreviation: 'PA'},
-    { name: 'PUERTO RICO', abbreviation: 'PR'},
-    { name: 'RHODE ISLAND', abbreviation: 'RI'},
-    { name: 'SOUTH CAROLINA', abbreviation: 'SC'},
-    { name: 'SOUTH DAKOTA', abbreviation: 'SD'},
-    { name: 'TENNESSEE', abbreviation: 'TN'},
-    { name: 'TEXAS', abbreviation: 'TX'},
-    { name: 'UTAH', abbreviation: 'UT'},
-    { name: 'VERMONT', abbreviation: 'VT'},
-    { name: 'VIRGIN ISLANDS', abbreviation: 'VI'},
-    { name: 'VIRGINIA', abbreviation: 'VA'},
-    { name: 'WASHINGTON', abbreviation: 'WA'},
-    { name: 'WEST VIRGINIA', abbreviation: 'WV'},
-    { name: 'WISCONSIN', abbreviation: 'WI'},
-    { name: 'WYOMING', abbreviation: 'WY' }
-];
-
-
-
-for(var i = 0; i < usStates.length; i++) {
-    var option = document.createElement("option");
-    option.text = usStates[i].name;
-    option.value = usStates[i].abbreviation;
-    state.add(option);
+const joke = async () => {
+  try {
+    const response = await fetch(`https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`)
+    const jokeData = await response.json()
+    const jokes = jokeData
+    // console.log(jokes)
+    createFragment1(jokes)
+    return jokes
+  } catch (e) {
+    console.log('ERROR!', e)
+  }
 }
-
-searchButton.addEventListener('click', (e) => {
-    let date = dateInput.value
-    let state = state.value
-    getData(date,state)
-})
-
-
